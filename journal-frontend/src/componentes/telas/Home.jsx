07466e1223d133a14b901556321420d7538ba7df
FelipeFontanaSeganfredo <felipefseganfredo@gmail.com>
2025-05-08 03:20:47 -0300

    Pequena alteração

--- journal-frontend/src/componentes/telas/Home.jsx
+++ journal-frontend/src/componentes/telas/Home.jsx
@@ -1,7 +1,7 @@
 const Home = () => (
     <div>
         <h1>Journal</h1>
-        <p>Seja bem vindo! Use esse espaço para registrar seus objetivos e fazer registros sobre seus dias!</p>
+        <p>Seja bem vindo! Use esse espaço para registrar seus objetivos e fazer registros sobre seus dias! Desenvolvido para a disciplina de pw1!</p>
     </div>
 );
 export default Home;
