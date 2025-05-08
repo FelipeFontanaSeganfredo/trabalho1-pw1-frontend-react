332b0b5b5a899b9f2d05555406cbaa20fd1e155f
FelipeFontanaSeganfredo <felipefseganfredo@gmail.com>
2025-05-07 22:41:17 -0300

    Arrumando imports para o deploy

--- journal-frontend/src/App.jsx
+++ journal-frontend/src/App.jsx
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import '@popperjs/core/dist/cjs/popper.js'
 import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-
 import Menu from './componentes/Menu'
 import Home from './componentes/telas/Home'
 import Sobre from "./componentes/telas/Sobre";
